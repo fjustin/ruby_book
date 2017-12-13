9d75bea98464e54ae293474d778d1a8a2e7029cc
futamidaiki <nimi0370@gmail.com>
2017-12-14 01:24:23 +0900

    仮実装と他のパターンの実装

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -1,9 +1,26 @@
 require 'minitest/autorun'
 require './lib/gate'
+require './lib/ticket'
 
 class GateTest < Minitest::Test
   def test_gate
-    #とりあえずGateオブジェクトが作れることを確認
-    assert Gate.new
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+
+    #trueになることを確認するためのassert
+    assert juso.exit(ticket)
+  end
+
+
+  def test_umeda_to_mikuni_when_fare_is_not_enough
+    umeda = Gate.new(:umeda)
+    mikuni = Gate.new(:mikuni)
+
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    refute mikuni.exit(ticket)
   end
 end
