233e24e4b0f11ff9b7f1ebb9d2952b45edcbcd1b
futamidaiki <nimi0370@gmail.com>
2018-01-07 01:00:19 +0900

    bankとteamファイルを追加し、配列をfreezeさせた

--- lib/deep_freezable.rb
+++ lib/deep_freezable.rb
@@ -1,5 +1,11 @@
 module DeepFreezable
   def deep_freeze(array_or_hash)
-    #実装はあとで
+    # 各要素の配列をfreeze
+    array_or_hash.each do |element|
+      element.freeze
+    end
+
+    # 配列自身をfreeze
+    array_or_hash.freeze
   end
 end
