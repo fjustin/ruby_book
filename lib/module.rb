132df5d304385321df8f92f7005da5a603df3058
futamidaiki <nimi0370@gmail.com>
2018-01-07 00:41:41 +0900

    Loggableモジュールをextendする

--- lib/module.rb
+++ lib/module.rb
@@ -1,3 +1,32 @@
+# モジュールの定義はincludeするときと同じ
+
+module Loggable
+  def log(text)
+    puts "[LOG] #{text}"
+  end
+end
+
+
+class Product
+  # Loggableモジュールのメソッドを特異メソッド(クラスメソッド)としてミックスインする
+  extend Loggable
+
+
+  def self.create_product(names)
+    #logメソッドをクラスメソッド内で呼び出す
+    log 'create_product is called'
+  end
+end
+
+
+#クラスメソッド経由でlogメソッドが呼び出される
+Product.create_product([])
+
+
+# Productクラスのクラスメソッドとして直接呼び出すことも可能
+Product.log('Hello!')
+
+=begin
 # ログ出力用のメソッドを提供するモジュール
  module Loggable
    def log(text)
@@ -42,7 +71,7 @@ puts user.name
 
 
 
-=begin
+
 class Product
   def title
     log 'title is called.'
