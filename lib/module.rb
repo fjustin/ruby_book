a6eb8a4d5da8ef8a3a503218af7af5708aa8a766
futamidaiki <nimi0370@gmail.com>
2018-01-06 22:41:58 +0900

    Loggableモジュールをincludeする

--- lib/module.rb
+++ lib/module.rb
@@ -1,3 +1,48 @@
+# ログ出力用のメソッドを提供するモジュール
+ module Loggable
+   def log(text)
+     puts "[LOG] #{text}"
+   end
+ end
+
+
+class Product
+  #上で作ったmoduleをincludeする
+  include Loggable
+
+  def title
+    # logメソッドはLoggableモジュールで定義したメソッド
+    log 'title is called'
+    'A great movie'
+  end
+end
+
+class User
+  # こちらも同じようにincludeする
+  include Loggable
+
+
+
+  def name
+    # Loggableモジュールのメソッドが使える
+    log 'name is called'
+    'Alice'
+  end
+end
+
+
+product = Product.new
+puts product.title
+
+
+user = User.new
+puts user.name
+
+
+
+
+
+=begin
 class Product
   def title
     log 'title is called.'
@@ -34,3 +79,5 @@ puts product.title
 
 user = User.new
 puts user.name
+
+=end
