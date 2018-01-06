aa74d5f495c8c7c83eb2e6160a6cf9d91e0071a3
futamidaiki <nimi0370@gmail.com>
2018-01-07 01:13:18 +0900

    ハッシュに対応

--- lib/deep_freezable.rb
+++ lib/deep_freezable.rb
@@ -1,10 +1,17 @@
 module DeepFreezable
   def deep_freeze(array_or_hash)
-    # 各要素の配列をfreeze
-    array_or_hash.each do |element|
-      element.freeze
+    case array_or_hash
+      when Array
+      # 各要素の配列をfreeze
+      array_or_hash.each do |element|
+        element.freeze
+      end
+      when Hash
+      array_or_hash.each do |key,value|
+        key.freeze
+        value.freeze
+      end
     end
-
     # 配列自身をfreeze
     array_or_hash.freeze
   end
