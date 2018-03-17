d579b611ca1cff02d9f751b746e2da174e9b2afc
futamidaiki <nimi0370@gmail.com>
2018-03-17 18:19:41 +0900

    Add echo effect

--- lib/effects.rb
+++ lib/effects.rb
@@ -1,4 +1,13 @@
 module Effects
+  def self.echo(rate)
+    ->(words) do
+      # スペースならそのまま返す
+      # スペース以外ならその文字を指定された回数だけ繰り返す
+      # 三項演算子の?を利用
+      words.chars.map{ |c| c == ' ' ?  c:c * rate}.join
+    end
+  end
+
   def self.reverse
     ->(words) do
       words.split(' ').map(&:reverse).join(' ')
--- test/effect_test.rb
+++ test/effect_test.rb
@@ -2,6 +2,14 @@ require 'minitest/autorun'
 require './lib/effects'
 
 class EffectsTest < Minitest::Test
+  def test_echo
+    effect = Effects.echo(2)
+    assert_equal 'RRuubbyy iiss ffuunn!!',effect.call('Ruby is fun!')
+
+    effect = Effects.echo(3)
+    assert_equal 'RRRuuubbbyyy iiisss fffuuunnn!!!',effect.call('Ruby is fun!')
+  end
+
   def test_reverse
     effect = Effects.reverse
     assert_equal 'ybuR si !nuf', effect.call('Ruby is fun!')
